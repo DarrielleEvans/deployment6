198af3708677cedc27aceb5ff4c2a0978d89d90d
Darrielle Evans <89504317+DarrielleEvans@users.noreply.github.com>
2023-11-28 18:13:38 -0600

    Update main.tf

--- initTerraform/main.tf
+++ initTerraform/main.tf
@@ -1,8 +1,8 @@
 #configure provider east
 #resource: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/resource-tagging
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region = var.region
   #profile = "Admin"
 }
